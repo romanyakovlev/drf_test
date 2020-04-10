290da41d4b427469af091357efdbf89434d1bc97
romanyakovlev <yakovlevroman8@gmail.com>
2020-04-10 22:24:58 +0700

    add post & like/dislike models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    text = models.CharField(max_length = 140)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now = True)
+
+
+class PostLike(models.Model):
+    person = models.ManyToManyField(User,related_name='person_liked')
+    tweet = models.ForeignKey(Post,null=True,related_name='likes', on_delete=models.CASCADE,)
+
+
+class PostUnlike(models.Model):
+    person = models.ManyToManyField(User,related_name='person_unliked')
+    tweet = models.ForeignKey(Post,null=True,related_name='unlikes', on_delete=models.CASCADE,)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+class HelloView(APIView):
+	def get(self, request):
+		content = {'message':'Hell0 World'}
+		return Response(content)
+		
